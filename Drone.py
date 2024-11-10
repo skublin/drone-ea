c279f75d125b565377c894c151e9f01473498d70
Liljack118 <kamilzajaczek4@gmail.com>
2024-11-10 12:16:01 +0100

    add camera movement

--- Drone.py
+++ Drone.py
@@ -7,23 +7,25 @@ class Drone:
         self.x = x  # X position
         self.y = y  # Y position
 
+        self.width = width
+
         imp1 = pygame.image.load("assets/drone-1.png").convert_alpha()
         imp2 = pygame.image.load("assets/drone-2.png").convert_alpha()
         # resize the drone image
-        height = int(imp1.get_height() * (width / imp1.get_width()))
-        self.imp1 = pygame.transform.scale(imp1, (width, height))
-        self.imp2 = pygame.transform.scale(imp2, (width, height))
+        self.height = int(imp1.get_height() * (width / imp1.get_width()))
+        self.imp1 = pygame.transform.scale(imp1, (width, self.height))
+        self.imp2 = pygame.transform.scale(imp2, (width, self.height))
         self.img = 1
 
         # Drone properties (needed for physics)
-        self.mass, self.drone_size, self.gravity = 1, 25, 0.1
+        self.mass, self.drone_size, self.gravity = 1, 30, 0.1
 
         # force of left and right thrusters
         self.left_thruster, self.right_thruster = 0.04, 0.04
 
         self.thruster_amplitude = 0.04  # Amplitude with which speed is increased
         self.thruster_diff_amplitude = (
-            0.003  # Amplitude with which speed between thrusters is decreased
+            0.002  # Amplitude with which speed between thrusters is decreased
         )
 
         # angle, speed responsible for drone rotation
@@ -54,6 +56,24 @@ class Drone:
         return (
             (self.right_thruster - self.left_thruster) * self.drone_size
         ) / self.mass
+
+    def display_x(self, surface, board_width):
+        # X position of the drone on the display
+
+        if self.x <= surface.get_width() / 2:
+            return self.x
+        elif self.x >= board_width - surface.get_width() / 2:
+            return self.x - (board_width - surface.get_width())
+        else:
+            return surface.get_width() / 2
+
+    def display_y(self, surface, board_height):
+        if self.y <= surface.get_height() / 2:
+            return self.y
+        elif self.y >= board_height - surface.get_height() / 2:
+            return self.y - (board_height - surface.get_height())
+        else:
+            return surface.get_height() / 2
 
     def _update_thruster_force(self, pressed: list[int]):
         # There can be multiple keys pressed at the same time
@@ -93,7 +113,7 @@ class Drone:
         self.y += self.y_speed
         self.angle += self.angular_speed
 
-    def draw(self, surface):
+    def draw(self, surface, board_width, board_height):
         # draw drone and rotate it based on angle
 
         if self.img == 1:
@@ -103,11 +123,14 @@ class Drone:
             drone = pygame.transform.rotate(self.imp2, self.angle)
             self.img = 1
 
+        display_x = self.display_x(surface, board_width)
+        display_y = self.display_y(surface, board_height)
+
         # get center of the drone
         drone_x, drone_y = (
-            self.x - drone.get_width() // 2,
-            self.y - drone.get_height() // 2,
+            display_x - drone.get_width() // 2,
+            display_y - drone.get_height() // 2,
         )
 
         surface.blit(drone, (drone_x, drone_y))
-        pygame.draw.circle(surface, (0, 0, 0), (self.x, self.y), 10)
+        pygame.draw.circle(surface, (0, 0, 0), (display_x, display_y), 10)
